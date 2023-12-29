ef2c0f321c5bc3f6e4fca0f32600d6812718748c
codinghub306 <154735143+codinghub306@users.noreply.github.com>
2023-12-29 22:21:47 +0530

    Add files via upload

---
+++ JannatMusic/Modules/skip.py
@@ -0,0 +1,55 @@
+from pyrogram import filters
+from pyrogram.types import Message
+from pytgcalls.types import AudioPiped, HighQualityAudio
+
+from JannatMusic import BOT_USERNAME, app, jannatdb, pytgcalls
+from JannatMusic.Helpers import _clear_, admin_check, buttons, close_key, gen_thumb
+
+
+@app.on_message(filters.command(["skip", "next"]) & filters.group)
+@admin_check
+async def skip_str(_, message: Message):
+    try:
+        await message.delete()
+    except:
+        pass
+    get = jannatdb.get(message.chat.id)
+    if not get:
+        try:
+            await _clear_(message.chat.id)
+            await pytgcalls.leave_group_call(message.chat.id)
+            await message.reply_text(
+                text=f"➻ sᴛʀᴇᴀᴍ sᴋɪᴩᴩᴇᴅ 🥺\n│ \n└ʙʏ : {message.from_user.mention} 🥀\n\n**» ɴᴏ ᴍᴏʀᴇ ǫᴜᴇᴜᴇᴅ ᴛʀᴀᴄᴋs ɪɴ** {message.chat.title}, **ʟᴇᴀᴠɪɴɢ ᴠɪᴅᴇᴏᴄʜᴀᴛ.**",
+                reply_markup=close_key,
+            )
+        except:
+            return
+    else:
+        title = get[0]["title"]
+        duration = get[0]["duration"]
+        file_path = get[0]["file_path"]
+        videoid = get[0]["videoid"]
+        req_by = get[0]["req"]
+        user_id = get[0]["user_id"]
+        get.pop(0)
+
+        stream = AudioPiped(file_path, audio_parameters=HighQualityAudio())
+        try:
+            await pytgcalls.change_stream(
+                message.chat.id,
+                stream,
+            )
+        except:
+            await _clear_(message.chat.id)
+            return await pytgcalls.leave_group_call(message.chat.id)
+
+        await message.reply_text(
+            text=f"➻ sᴛʀᴇᴀᴍ sᴋɪᴩᴩᴇᴅ 🥺\n│ \n└ʙʏ : {message.from_user.mention} 🥀",
+            reply_markup=close_key,
+        )
+        img = await gen_thumb(videoid, user_id)
+        return await message.reply_photo(
+            photo=img,
+            caption=f"**➻ sᴛᴀʀᴛᴇᴅ sᴛʀᴇᴀᴍɪɴɢ**\n\n‣ **ᴛɪᴛʟᴇ :** [{title[:27]}](https://t.me/{BOT_USERNAME}?start=info_{videoid})\n‣ **ᴅᴜʀᴀᴛɪᴏɴ :** `{duration}` ᴍɪɴᴜᴛᴇs\n‣ **ʀᴇǫᴜᴇsᴛᴇᴅ ʙʏ :** {req_by}",
+            reply_markup=buttons,
+        )
