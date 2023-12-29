9e1890bd4c63199f2a11468e0be3c46de307d47b
Mr Raj <155147556+CodeLearning301@users.noreply.github.com>
2023-12-29 19:28:44 +0530

    Update sessiongen.py

--- sessiongen.py
+++ sessiongen.py
@@ -8,14 +8,14 @@ API_HASH = input("\nEnter Your API HASH :\n > ")
 
 print("\n\nEnter the phone number associated with your telegram account when asked.\n\n")
 
-fallen = Client("Fallen", api_id=API_ID, api_hash=API_HASH, in_memory=True)
+fallen = Client("Jannat", api_id=API_ID, api_hash=API_HASH, in_memory=True)
 
 
 async def main():
     await fallen.start()
-    sess = await fallen.export_session_string()
-    txt = f"Here is your Pyrogram {ver} String Session\n\n<code>{sess}</code>\n\nDon't share it with anyone.\nDon't forget to join @FallenAssociation"
-    ok = await fallen.send_message("me", txt)
+    sess = await jannat.export_session_string()
+    txt = f"Here is your Pyrogram {ver} String Session\n\n<code>{sess}</code>\n\nDon't share it with anyone.\nDon't forget to join @jannatupdate"
+    ok = await jannat.send_message("me", txt)
     print(f"Here is your Pyrogram {ver} String Session\n{sess}\nDouble click to copy.") 
 
 
