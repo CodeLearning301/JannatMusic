5316d2eb72b621e8a98bed856e0b9b7c16a9d7a8
codinghub306 <154735143+codinghub306@users.noreply.github.com>
2023-12-29 22:20:47 +0530

    Add files via upload

---
+++ JannatMusic/Modules/resume.py
@@ -0,0 +1,23 @@
+from pyrogram import filters
+from pyrogram.types import Message
+
+from JannatMusic import app, pytgcalls
+from JannatMusic.Helpers import admin_check, close_key, is_streaming, stream_on
+
+
+@app.on_message(filters.command(["resume"]) & filters.group)
+@admin_check
+async def res_str(_, message: Message):
+    try:
+        await message.delete()
+    except:
+        pass
+
+    if await is_streaming(message.chat.id):
+        return await message.reply_text("ᴅɪᴅ ʏᴏᴜ ʀᴇᴍᴇᴍʙᴇʀ ᴛʜᴀᴛ ʏᴏᴜ ᴘᴀᴜsᴇᴅ ᴛʜᴇ sᴛʀᴇᴀᴍ ?")
+    await stream_on(message.chat.id)
+    await pytgcalls.resume_stream(message.chat.id)
+    return await message.reply_text(
+        text=f"➻ sᴛʀᴇᴀᴍ ʀᴇsᴜᴍᴇᴅ 💫\n│ \n└ʙʏ : {message.from_user.mention} 🥀",
+        reply_markup=close_key,
+    )
