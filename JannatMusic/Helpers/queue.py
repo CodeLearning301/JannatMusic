8c869824e9da139a9ef70ede8cce7423ddd6c099
codinghub306 <154735143+codinghub306@users.noreply.github.com>
2023-12-29 23:44:28 +0530

    Add files via upload

---
+++ JannatMusic/Helpers/queue.py
@@ -0,0 +1,26 @@
+from JannatMusic import jannatdb
+
+
+async def put(
+    chat_id,
+    title,
+    duration,
+    videoid,
+    file_path,
+    ruser,
+    user_id,
+):
+    put_f = {
+        "title": title,
+        "duration": duration,
+        "file_path": file_path,
+        "videoid": videoid,
+        "req": ruser,
+        "user_id": user_id,
+    }
+    get = jannatdb.get(chat_id)
+    if get:
+        jannatdb[chat_id].append(put_f)
+    else:
+        jannatdb[chat_id] = []
+        jannatdb[chat_id].append(put_f)
