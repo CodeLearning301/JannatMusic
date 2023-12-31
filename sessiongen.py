80ce38de277125d27403d1fa44ced8c9c67f5e73
Mr Raj <155147556+CodeLearning301@users.noreply.github.com>
2023-12-31 08:40:11 +0530

    jannat

--- sessiongen.py
+++ sessiongen.py
@@ -8,11 +8,11 @@ API_HASH = input("\nEnter Your API HASH :\n > ")
 
 print("\n\nEnter the phone number associated with your telegram account when asked.\n\n")
 
-fallen = Client("Jannat", api_id=API_ID, api_hash=API_HASH, in_memory=True)
+jannat = Client("Jannat", api_id=API_ID, api_hash=API_HASH, in_memory=True)
 
 
 async def main():
-    await fallen.start()
+    await jannat.start()
     sess = await jannat.export_session_string()
     txt = f"Here is your Pyrogram {ver} String Session\n\n<code>{sess}</code>\n\nDon't share it with anyone.\nDon't forget to join @jannatupdate"
     ok = await jannat.send_message("me", txt)
